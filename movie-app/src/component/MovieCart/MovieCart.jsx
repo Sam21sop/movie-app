9dfb5011cf8513f2d42574e9bc232ec61bfb5413
Sam21sop <sbussiness21@gmail.com>
2024-02-18 18:26:42 +0530

    State in react

--- movie-app/src/component/MovieCart/MovieCart.jsx
+++ movie-app/src/component/MovieCart/MovieCart.jsx
@@ -2,26 +2,45 @@ import { Component } from "react";
 import './movieCart.css';
 
 export default class MovieCart extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            imgUrl: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+            title: "The Avenger",
+            plot: "This are real hero to destro everything.",
+            price: 999,
+            rating: 4.9
+        }
+    }
+
+
     render(){
+        const {imgUrl, title, plot, price, rating} = this.state;
         return (
             <>
                 <div className="main">
                     <div className="movie-cart">
                         {/* left part for poster  */}
                         <div className="left">
-                            <img src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" alt="poster" srcset="" />
+                            <img src={imgUrl} alt="poster" srcset="" />
                         </div>
 
                         {/* right part for other element */}
                         <div className="right">
-                            <div className="title">Title</div>
-                            <div className="plot">Plot</div>
-                            <div className="price">Rs. 199</div>
+                            <div className="title">{title}</div>
+                            <div className="plot">{plot}</div>
+                            <div className="price">Rs. {price}</div>
 
                             {/* footer of the right container */}
                             <div className="footer">
-                                <div className="rating">IMDB 4.5</div>
-                                <div className="stars star-dis">Stars</div>
+                                <div className="rating">IMDB {rating}</div>
+                                <div className="star-dis">
+                                    {/* <img src="https://cdn-icons-png.flaticon.com/128/992/992683.png" alt="decreament" className="inc-dec"/> */}
+                                    <img src="https://cdn-icons-png.flaticon.com/128/477/477406.png" alt="Star" className="stars"/>
+                                    {/* <img src="https://cdn-icons-png.flaticon.com/128/992/992651.png" alt="increament" className="inc-dec"/> */}
+                                    {/* <span className="stars">0</span> */}
+                                </div>
                                 <button className="fav-btn">Favourite</button>
                                 <button className="cart-btn">Add to Cart</button>
                             </div>
